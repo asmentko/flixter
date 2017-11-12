a8ea70f0917510cc3c616160482cee286d229f2c
asmentko <asmentko@gmail.com>
2017-11-12 20:14:49 +0000

    Added in picture uploading and aws server access

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,7 @@ class Course < ApplicationRecord
   belongs_to :user
   has_many :sections
 
+  mount_uploader :picture, PictureUploader
 
   validates :title, presence: true
   validates :description, presence: true
